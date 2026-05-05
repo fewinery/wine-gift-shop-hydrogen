95db6d632a3b9c2b435bc48ed10efb72b82e979d
fewinery <support@fairwindsestatewinery.com>
2026-05-05 13:11:28 -0600

    feat(newsletter): align campaign form with default form capabilities using shared component props

--- app/sections/newsletter/newsletter-campaign.tsx
+++ app/sections/newsletter/newsletter-campaign.tsx
@@ -5,10 +5,12 @@ import NewsLetterForm from "./newsletter-form";
 interface Props extends HydrogenComponentProps {
   buttonText?: string;
   placeholder?: string;
+  width?: number;
+  helpText?: string;
 }
 
 const EmailFormCampaign = forwardRef<HTMLDivElement, Props>((props, ref) => {
-  const { buttonText, placeholder, ...rest } = props;
+  const { buttonText, placeholder, width, helpText, ...rest } = props;
 
   return (
     <div ref={ref} {...rest}>
@@ -16,6 +18,8 @@ const EmailFormCampaign = forwardRef<HTMLDivElement, Props>((props, ref) => {
         formType="campaign"
         buttonText={buttonText || "Get Access"}
         placeholder={placeholder || "Enter your email"}
+        width={width || 500}
+        helpText={helpText}
       />
     </div>
   );
@@ -31,16 +35,37 @@ export const schema = createSchema({
       group: "Form",
       inputs: [
         {
-          type: "text",
-          name: "buttonText",
-          label: "Button text",
-          defaultValue: "Get Access",
+          type: "range",
+          name: "width",
+          label: "Input width",
+          configs: {
+            min: 300,
+            max: 600,
+            step: 10,
+            unit: "px",
+          },
+          defaultValue: 500,
         },
         {
           type: "text",
           name: "placeholder",
           label: "Placeholder",
           defaultValue: "Enter your email",
+          placeholder: "Enter your email",
+        },
+        {
+          type: "richtext",
+          name: "helpText",
+          label: "Help text",
+          defaultValue:
+            '<div>We care about the protection of your data. Read our <a href="/policies/privacy-policy" style="color: #007AFF; text-decoration: underline;">Privacy Policy</a>.</div>',
+        },
+        {
+          type: "text",
+          name: "buttonText",
+          label: "Button text",
+          placeholder: "Subscribe",
+          defaultValue: "Get Access",
         },
       ],
     },

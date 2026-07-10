ee28dc973411bba36d7bd8b4793ea69cf624967d
fewinery <support@fairwindsestatewinery.com>
2026-07-10 16:42:20 -0600

    fix(contact): remove stray JSX character

--- app/sections/contact-form/index.tsx
+++ app/sections/contact-form/index.tsx
@@ -77,8 +77,7 @@ function ContactForm(props: ContactFormProps) {
             autoComplete="off"
           />
         </div>
-        
-      >
+
         <div className="space-y-6">
           <div className="grid gap-6 md:grid-cols-2">
             {/* First Name */}
